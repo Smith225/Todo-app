f02d5a81dd293968405572b64cf726b7758f6f56
Smith225 <smithrf123@gmail.com>
2023-08-17 20:38:45 +0530

    Completed setting up react files and basic Home page.

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function Home(){
+
+    const [item, setItem]= useState("");
+
+
+    function handleClick(){
+
+        const ele= document.getElementsByClassName("todoItem");
+        console.log(ele[0].value);
+        setItem(ele[0].value);
+    }
+
+    return(
+        <>
+
+            <link rel="stylesheet" href={require("../css/styles.css")} />
+
+            <div className="box">
+
+                <h1>To - List</h1>
+
+                <input className="todoItem" placeholder="To do"></input>
+                <button className="sub" onClick={handleClick}>+</button>
+                <h1>{item}</h1>
+            </div>
+        </>   
+    )
+}
+
+export default Home;
